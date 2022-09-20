b4bdbc628d6b64a487d504a981633c20ac31f94f
Elybourland <JEBourland@yahoo.com>
2022-09-20 10:50:04 -0700

    change to globally scoped interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,8 +8,7 @@ import {
   Query, 
   Delete, 
   NotFoundException, 
-  Session,
-  UseInterceptors
+  Session
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -18,12 +17,10 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './user.entity';
 
 @Controller('auth')
 @Serialize(UserDto)
-@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService, 
