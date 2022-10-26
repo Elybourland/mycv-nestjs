aa5c9cee7e4fa7586d47083e7a7f562e551d1baf
Elybourland <JEBourland@yahoo.com>
2022-10-26 13:17:29 -0700

    applying a globally scoped middleware

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('Authentication System', () => {
   });
 
   it('handles a signup request', () => {
-    const email = 'asdf@asdf.com'
+    const email = 'asdf@asdf32.com'
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email: email, password: 'asdfjkl' })
